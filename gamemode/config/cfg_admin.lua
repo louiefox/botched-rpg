34ea0f71f7f4a02868a49b8e7a4891628e325310
AlexDotCore <64563384+dotCore-off@users.noreply.github.com>
2022-02-15 06:28:56 +0100

    feat: new dedicated config file for admin

---
+++ gamemode/config/cfg_admin.lua
@@ -0,0 +1,22 @@
+// Use gMMO privilege system
+BOTCHED.CONFIG.UseInBuiltSystem = true
+
+// Use an external administration system
+BOTCHED.CONFIG.UseThirdPartyMods = false
+
+/*
+             gMMO - Built-in privilege system
+            Put SteamID / SteamID64 of GMs here
+    Use if you set BOTCHED.CONFIG.UseInBuiltSystem to true
+*/
+BOTCHED.CONFIG.GameMasters = {
+    "76561198195008670"
+}
+
+/*
+        Put allowed admin ranks here (case sensitive)
+    Use if you set BOTCHED.CONFIG.UseThirdPartyMods to false
+*/
+BOTCHED.CONFIG.ThirdPartyRanks = {
+    "owner"
+}
